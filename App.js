23a51efe298658e4554caa9d4d53eb91c00e0d9c
Victor Gabriel <victorgabriel08fs@gmail.com>
2023-09-25 16:31:29 -0300

    first

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { api } from './services/api';
+import { ScrollView } from 'react-native-web';
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    await api.get('users').then((res) => {
+      setUsers(res.data);
+    })
+  }
+  useEffect(() => {
+    getUsers();
+  }, [users]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ScrollView>
+        {users.map((user) => (<Text style={styles.text}>
+          {user.name}
+        </Text>))}
+      </ScrollView>
+      <Text>Thiago cu sujo!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +32,13 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
+  text: {
+    color: '#e32012',
+    fontWeight: 'bold',
+    borderRadius: '5px',
+    padding: '5px',
+    fontSize:'35px'
+  }
 });
